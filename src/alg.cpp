6187cd545f96e8eecec4d7b366411b7043d228e0
CharlyArs <secretshadow123456@gmail.com>
2026-04-19 23:04:37 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,106 +1,85 @@
 // Copyright 2026 NNTU-CS
-#include <iostream>
 #include <string>
-#include <map>
 #include "tstack.h"
 
 int getPriority(const char op) {
-    if (op == '(')
-        return 0;
-    else if (op == ')')
-        return 1;
-    else if (op == '+' || op == '-')
-        return 2;
-    else if (op == '*' || op == '/')
-        return 3;
-    else
-        throw "Error symbol";
+  if (op == '(') return 0;
+  if (op == ')') return 1;
+  if (op == '+' || op == '-') return 2;
+  if (op == '*' || op == '/') return 3;
+  return -1;
 }
 
 std::string infx2pstfx(const std::string& inf) {
-    TStack<char, 100> opStack;
-    int i = 0;
-    std::string postfix;
+  TStack<char, 100> opStack;
+  std::string postfix;
 
-    while (i < inf.length()) {
-        if (inf[i] >= '0' && inf[i] <= '9') {
-            while (i < inf.length() && inf[i] >= '0' && inf[i] <= '9') {
-                postfix += inf[i];
-                i++;
-            }
-            postfix += ' ';
-            continue;
-        }
+  for (size_t i = 0; i < inf.length(); ++i) {
+    char ch = inf[i];
 
-        int priority = getPriority(inf[i]);
+    if (ch == ' ') continue;
 
-        if (priority == 0) { 
-            opStack.push(inf[i]);
-        } 
-        else if (priority == 1) { 
-            while (!opStack.isEmpty() && getPriority(opStack.get()) != 0) {
-                postfix += opStack.get();
-                postfix += ' ';
-                opStack.pop();
-            }
-            opStack.pop(); // Удаляем '('
-        } 
-        else if (priority > 1) { // Операторы +, -, *, /
-            if (opStack.isEmpty() || priority > getPriority(opStack.get())) {
-                opStack.push(inf[i]);
-            } else {
-                while (!opStack.isEmpty() && opStack.get() != '(' &&
-                       priority <= getPriority(opStack.get())) {
-                    postfix += opStack.get();
-                    postfix += ' ';
-                    opStack.pop();
-                }
-                opStack.push(inf[i]);
-            }
-        }
-        i++;
-    }
-
-    while (!opStack.isEmpty()) {
+    if (ch >= '0' && ch <= '9') {
+      while (i < inf.length() && inf[i] >= '0' && inf[i] <= '9') {
+        postfix += inf[i++];
+      }
+      postfix += ' ';
+      i--;
+    } else if (ch == '(') {
+      opStack.push(ch);
+    } else if (ch == ')') {
+      while (!opStack.isEmpty() && opStack.get() != '(') {
         postfix += opStack.get();
         postfix += ' ';
         opStack.pop();
+      }
+      opStack.pop();
+    } else {
+      int priority = getPriority(ch);
+      while (!opStack.isEmpty() && getPriority(opStack.get()) >= priority) {
+        postfix += opStack.get();
+        postfix += ' ';
+        opStack.pop();
+      }
+      opStack.push(ch);
     }
-    return postfix;
+  }
+
+  while (!opStack.isEmpty()) {
+    postfix += opStack.get();
+    postfix += ' ';
+    opStack.pop();
+  }
+
+  if (!postfix.empty() && postfix.back() == ' ') {
+    postfix.pop_back();
+  }
+  return postfix;
 }
 
 int eval(const std::string& pref) {
-    TStack<int, 100> valStack;
-    int currentRes = 0;
-    int pos = 0;
+  TStack<int, 100> valStack;
+  for (size_t i = 0; i < pref.length(); ++i) {
+    if (pref[i] == ' ') continue;
 
-    while (pos < pref.length()) {
-        if (pref[pos] >= '0' && pref[pos] <= '9') {
-            std::string numberStr;
-            while (pref[pos] != ' ') {
-                numberStr += pref[pos];
-                pos++;
-            }
-            pos++;
-            valStack.push(std::stoi(numberStr));
-            continue;
-        }
-        
-        currentRes = valStack.get();
-        valStack.pop();
-        if (pref[pos] == '+') { 
-            currentRes += valStack.get();
-        } else if (pref[pos] == '-') {
-            currentRes = valStack.get() - currentRes;
-        } else if (pref[pos] == '*') {
-            currentRes *= valStack.get();
-        } else if (pref[pos] == '/') {
-            currentRes = valStack.get() / currentRes;
-        }
-        valStack.pop();
-        valStack.push(currentRes);
-        currentRes = 0;
-        pos += 2;
+    if (pref[i] >= '0' && pref[i] <= '9') {
+      std::string num;
+      while (i < pref.length() && pref[i] >= '0' && pref[i] <= '9') {
+        num += pref[i++];
+      }
+      valStack.push(std::stoi(num));
+      i--;
+    } else {
+      int v2 = valStack.get();
+      valStack.pop();
+      int v1 = valStack.get();
+      valStack.pop();
+
+      if (pref[i] == '+') valStack.push(v1 + v2);
+      else if (pref[i] == '-') valStack.push(v1 - v2);
+      else if (pref[i] == '*') valStack.push(v1 * v2);
+      else if (pref[i] == '/') valStack.push(v1 / v2);
     }
-    return valStack.get();
+  }
+  return valStack.get();
 }
