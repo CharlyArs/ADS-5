deaefbd0fb590778fdfd909c6105fa6e17d9af7f
CharlyArs <secretshadow123456@gmail.com>
2026-04-19 22:43:59 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -1,10 +1,44 @@
-// Copyright 2021 NNTU-CS
+// Copyright 2026 NNTU-CS
 #ifndef INCLUDE_TSTACK_H_
 #define INCLUDE_TSTACK_H_
 
 template<typename T, int size>
 class TStack {
-  // добавьте код стека
+ private:
+  T storage[size];
+  int topIndex;
+
+ public:
+  TStack() : topIndex(-1) {}
+
+  int getSize() const { 
+      return topIndex + 1; 
+  }
+
+  bool isEmpty() const { 
+      return topIndex == -1; 
+  }
+
+  void push(const T& item) {
+    if (topIndex < size - 1)
+      storage[++topIndex] = item;
+    else
+      throw "Full stack";
+  }
+
+  T get() const {
+    if (topIndex >= 0)
+      return storage[topIndex];
+    else
+      throw "Empty stack";
+  }
+
+  void pop() {
+    if (topIndex >= 0)
+      topIndex--;
+    else
+      throw "Empty stack";
+  }
 };
 
 #endif  // INCLUDE_TSTACK_H_
